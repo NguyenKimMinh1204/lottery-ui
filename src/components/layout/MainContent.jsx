295bd38504ef81b554440df580f7b31e84587d8f
Nguyen Kim Minh <kimminh01102002@gmail.com>
2025-10-14 17:12:32 +0700

    xoá demo

--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -1,7 +1,7 @@
 export default function MainContent() {
   return (
-    <main className="bg-pink-300 min-h-[60vh] flex items-center justify-center">
-      <div className="container text-center text-lg font-semibold">
+    <main className="bg-pink-300 min-h-[60vh] flex items-center justify-center font-gilroy">
+      <div className="container text-center text-lg">
         Main content
       </div>
     </main>
